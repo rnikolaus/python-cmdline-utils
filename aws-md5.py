079f4fe320c7a2a9489da31919f312adc2bcfedd
rnikolaus <rnikolaus@users.noreply.github.com>
2016-06-19 21:32:09 +0200

    aws

---
+++ aws-md5.py
@@ -0,0 +1,14 @@
+'''
+Created on 19.06.2016
+
+@author: rapnik
+'''
+import boto3
+# Let's use Amazon S3
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    #sizesum=0
+    for key in bucket.objects.all():
+        print key.key, key.e_tag.replace('"',''),key.size
+        sizesum+=key.size
+    #print sizesum
